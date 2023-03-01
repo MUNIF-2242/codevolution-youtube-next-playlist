9ccc011b446fff007cf4bf0cc40167aaefd68990
MUNIF <munif.shellbeehaken@gmail.com>
2023-03-01 17:07:08 +0600

    complete API POST request

--- pages/api/shopping-lists/index.js
+++ pages/api/shopping-lists/index.js
@@ -1,5 +1,15 @@
 import { shoppingLists } from '@/data/shopping-list';
 
 export default function handler(req, res) {
-  res.status(200).json(shoppingLists);
+  if (req.method === 'GET') {
+    res.status(200).json(shoppingLists);
+  } else if (req.method === 'POST') {
+    const shoppingList = req.body.shoppingList;
+    const newShoppingList = {
+      id: Date.now(),
+      title: shoppingList,
+    };
+    shoppingLists.push(newShoppingList);
+    res.status(201).json(newShoppingList);
+  }
 }
--- pages/shopping-lists/index.js
+++ pages/shopping-lists/index.js
@@ -2,13 +2,32 @@ import { useState } from 'react';
 
 function ShoppingListsPage() {
   const [shoppingLists, setShoppingLists] = useState([]);
+  const [shoppingList, setShoppingList] = useState('');
   const fetchShoppingLists = async () => {
     const response = await fetch('/api/shopping-lists');
     const data = await response.json();
     setShoppingLists(data);
   };
+
+  const submitShoppingList = async () => {
+    const response = await fetch('/api/shopping-lists', {
+      method: 'POST',
+      body: JSON.stringify({ shoppingList }),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
   return (
     <>
+      <input
+        type='text'
+        value={shoppingList}
+        onChange={(e) => setShoppingList(e.target.value)}
+      />
+      <button onClick={submitShoppingList}>Add</button>
       <button onClick={fetchShoppingLists}>Load Shopping Lists</button>
       {shoppingLists.map((shoppingList) => (
         <li key={shoppingList.id}>{shoppingList.title}</li>
